2f3bf1f9beb347670aae3d1e53617ca7d1b110e4
Florian Renfer <florian.renfer@heyoptima.de>
2026-04-07 08:27:49 +0200

    feat(session): add redis session

--- api/src/main/java/com/github/florian_renfer/testcontainers/TestcontainersApplication.java
+++ api/src/main/java/com/github/florian_renfer/testcontainers/TestcontainersApplication.java
@@ -2,12 +2,13 @@ package com.github.florian_renfer.testcontainers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class TestcontainersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestcontainersApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TestcontainersApplication.class, args);
+  }
 }
